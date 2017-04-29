40160da9b0184753aebf28c1242183f2867dc9f4
Boris Shpilyuck <boris.shpilyuck@emc.com>
2017-04-29 17:39:38 +0300

    Automation maven config produces self contained shaded JAR

---
+++ automation/team-space-automation/src/main/resources/scripts/tsa-start.bat
@@ -0,0 +1,2 @@
+java -cp team-space-automation-1.0-SNAPSHOT.jar org.testng.TestNG testng.xml
+pause
---
+++ server/team-space-server/src/main/resources/scripts/tss-start.bat
@@ -0,0 +1,2 @@
+java -Ddw.server.applicationConnectors[1].keyStorePath=./keys/keystore.jks -jar team-space-server-1.0-SNAPSHOT.jar server config.yml
+pause
