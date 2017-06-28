838ab29bcaf896263b8d12460aac333e54947b03
Boris Shpilyuck <boris.shpilyuck@emc.com>
2017-06-28 15:39:43 +0300

    MySQL automatic install and configuration on CentOS instance

--- deploy/team-space-deploy-aws/src/main/resources/db_user_data.sh
+++ deploy/team-space-deploy-aws/src/main/resources/db_user_data.sh
@@ -22,6 +22,43 @@ sed -ie 's/#PasswordAuthentication no//g' /etc/ssh/sshd_config
 service sshd reload
 echo "Completed sshd service reload at: "$(date +"%T") >> $log
 
+cd /home/$user$
+sudo wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+echo "Completed MySQl rpm download at: "$(date +"%T") >> $log
+sudo yum -y localinstall mysql57-community-release-el7-7.noarch.rpm
+echo "Completed MySQl local install at: "$(date +"%T") >> $log
+sudo yum -y install mysql-community-server
+echo "Completed MySQl install at: "$(date +"%T") >> $log
+sudo service mysqld start
+echo "Started MySQl at: "$(date +"%T") >> $log
+
+
+cat >dbsetup.sql <<EOF
+uninstall plugin validate_password;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE teamspace;
+CREATE USER '$user$'@'localhost' IDENTIFIED BY '$pass$';
+GRANT ALL PRIVILEGES ON * . * TO '$user$'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+sudo systemctl stop mysqld
+sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+sudo systemctl start mysqld
+
+mysql -sfu root -e "UPDATE mysql.user SET authentication_string = PASSWORD('root11'), password_expired = 'N' WHERE User = 'root' AND Host = 'localhost'"
+echo "Changed root password at: "$(date +"%T") >> $log
+
+sudo systemctl stop mysqld
+sudo systemctl unset-environment MYSQLD_OPTS
+sudo systemctl start mysqld
+
+mysql -sfu root -proot11< "dbsetup.sql"
+echo "Applied dbsetup.sql script at: "$(date +"%T") >> $log
+
+
+
 chown -R $user$:$user$ /home/$user$
 sudo su - $user$
 echo "Completed deploy at: "$(date +"%T") >> $log
